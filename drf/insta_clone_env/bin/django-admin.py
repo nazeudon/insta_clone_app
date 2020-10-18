ca370a58a0c4ccc81539d206720c4ed3bde20f02
nazeudon <nazeudon48112@icloud.com>
2020-10-18 10:28:18 +0900

    01_create_django_project

---
+++ drf/insta_clone_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/hnakatani/Documents/その他/Learning/JavaScript/Udemy/insta_clone_app/drf/insta_clone_env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
