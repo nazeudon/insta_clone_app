bbad3f8bf4d1e15d5e665846d553204e765f21a5
nazeudon <nazeudon48112@icloud.com>
2020-10-18 22:08:08 +0900

    02_User_model

--- drf/api/models.py
+++ drf/api/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser, BaseUserManager, PermissionsMixin
+)
+from django.conf import settings
 
-# Create your models here.
+
+# 元々djangomが提供しているクラスをemail認証用に書き換える
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError('email is must!')
+
+        user = self.model(email=self.normalize_email(email))
+        user.set_password(password)  # 裏でハッシュ化も行われている
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=50, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"  # defaultだと"username"になっている
+
+    def __str__(self):
+        return self.email
