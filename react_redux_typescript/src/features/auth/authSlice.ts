2ad32b20a0e1ba974c66fb0e26741083baa6fb5e
nazeudon <nazeudon48112@icloud.com>
2020-10-22 10:12:06 +0900

    12_authSlice_createAsyncThunk

--- react_redux_typescript/src/features/auth/authSlice.ts
+++ react_redux_typescript/src/features/auth/authSlice.ts
@@ -2,11 +2,92 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { RootState } from "../../app/store";
 import axios from "axios";
 import { PROPS_AUTHEN, PROPS_NICKNAME, PROPS_PROFILE } from "../types";
-import { Tune } from "@material-ui/icons";
+import { profile } from "console";
 
 // .envファイルにパスを指定
 // process.envにつなぐことでパスとして認識してくれる
 const apiUrl = process.env.REACT_APP_DEV_API_URL;
+
+//ログイン
+export const fetchAsyncLogin = createAsyncThunk(
+  "auth/post",
+  async (authen: PROPS_AUTHEN) => {
+    const res = await axios.post(`${apiUrl}authen/jwt/create`, authen, {
+      headers: {
+        "Content-Type": "application/json", //postの場合指定する
+      },
+    });
+    return res.data;
+  }
+);
+
+//新規ユーザ
+export const fetchAsyncRegister = createAsyncThunk(
+  "auth/register",
+  async (auth: PROPS_AUTHEN) => {
+    const res = await axios.post(`${apiUrl}api/register/`, auth, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return res.data;
+  }
+);
+
+//プロフィール作成
+export const fetchAsyncCreateProf = createAsyncThunk(
+  "profile/post",
+  async (nickName: PROPS_NICKNAME) => {
+    const res = await axios.post(`${apiUrl}api/profile/`, nickName, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `JWT ${localStorage.localJWT}`,
+      },
+    });
+    return res.data;
+  }
+);
+
+// プロフィール更新
+export const fetchAsyncUpdateProf = createAsyncThunk(
+  "profile/put",
+  async (profile: PROPS_PROFILE) => {
+    const uploadData = new FormData();
+    uploadData.append("nickName", profile.nickName);
+    profile.img && uploadData.append("img", profile.img, profile.img.name);
+    const res = await axios.put(
+      `${apiUrl}api/profile/${profile.id}/`,
+      uploadData,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `JWT ${localStorage.localJWT}`,
+        },
+      }
+    );
+    return res.data;
+  }
+);
+
+//ログインユーザの取得
+export const fetchAsyncGetMyProf = createAsyncThunk("profile/get", async () => {
+  const res = await axios.get(`${apiUrl}api/myprofile/`, {
+    headers: {
+      Authorization: `JWT ${localStorage.localJWT}`,
+    },
+  });
+  return res.data[0];
+});
+
+//存在するプロフィールの取得
+export const fetchAsyncGetProfs = createAsyncThunk("profiles/get", async () => {
+  const res = await axios.get(`${apiUrl}api/profile/`, {
+    headers: {
+      Authorization: `JWT ${localStorage.localJWT}`,
+    },
+  });
+  return res.data;
+});
 
 export const authSlice = createSlice({
   name: "auth",
