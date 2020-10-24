39e983591059ed4541b526941bc67ad3d5c377dd
nazeudon <nazeudon48112@icloud.com>
2020-10-24 11:01:12 +0900

    16_login_modal

--- react_redux_typescript/src/features/auth/Auth.tsx
+++ react_redux_typescript/src/features/auth/Auth.tsx
@@ -142,11 +142,115 @@ const Auth: React.FC = () => {
                   <span
                     className={styles.auth_text}
                     onClick={async () => {
-                      await dispatch(setOpenSignIn());
                       await dispatch(resetOpenSignUp());
+                      await dispatch(setOpenSignIn());
                     }}
                   >
                     You already have an acount ?
+                  </span>
+                </div>
+              </form>
+            </div>
+          )}
+        </Formik>
+      </Modal>
+
+      <Modal //ログイン用モーダル
+        isOpen={openSignIn}
+        //モーダル以外の箇所をクリックした時
+        onRequestClose={async () => {
+          await dispatch(resetOpenSignIn());
+        }}
+        style={customStyles}
+      >
+        <Formik
+          initialErrors={{ email: "required" }}
+          initialValues={{ email: "", password: "" }}
+          onSubmit={async (values) => {
+            await dispatch(fetchCredStart());
+            const resultReg = await dispatch(fetchAsyncLogin(values));
+
+            if (fetchAsyncLogin.fulfilled.match(resultReg)) {
+              await dispatch(fetchAsyncGetProfs());
+              await dispatch(fetchAsyncGetMyProf());
+            }
+            await dispatch(fetchCredEnd);
+            await dispatch(resetOpenSignIn);
+          }}
+          validationSchema={Yup.object().shape({
+            email: Yup.string()
+              .email("emial format is wrong")
+              .required("email is must"),
+            password: Yup.string().required("passwrod is must").min(4),
+          })}
+        >
+          {({
+            handleSubmit,
+            handleChange,
+            handleBlur,
+            values,
+            errors,
+            touched,
+            isValid,
+          }) => (
+            <div>
+              <form onSubmit={handleSubmit}>
+                <div className={styles.auth_signUp}>
+                  <h1 className={styles.auth_title}>SNS clone</h1>
+                  <br />
+                  {/* クルクルアイコン */}
+                  <div className={styles.auth_progress}>
+                    {isLoadingAuth && <CircularProgress />}
+                  </div>
+                  <br />
+
+                  <TextField
+                    placeholder="email"
+                    type="input"
+                    name="email"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.email}
+                  />
+                  <br />
+                  {touched.email && errors.email ? (
+                    <div className={styles.auth_error}>{errors.email}</div>
+                  ) : null}
+
+                  <TextField
+                    placeholder="password"
+                    type="input"
+                    name="password"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.password}
+                  />
+                  <br />
+                  {touched.password && errors.password ? (
+                    <div className={styles.auth_error}>{errors.password}</div>
+                  ) : null}
+                  <br />
+                  <br />
+
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    disabled={!isValid}
+                    type="submit"
+                  >
+                    Login
+                  </Button>
+                  <br />
+                  <br />
+
+                  <span
+                    className={styles.auth_text}
+                    onClick={async () => {
+                      await dispatch(resetOpenSignIn());
+                      await dispatch(setOpenSignUp());
+                    }}
+                  >
+                    You don't have an account ?
                   </span>
                 </div>
               </form>
